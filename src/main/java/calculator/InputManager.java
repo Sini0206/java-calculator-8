b46dfddc407ca941deb296c24e9bdedb3271a732
Sini0206 <a01092439672@gmail.com>
2025-10-20 02:37:38 +0900

    feat: 커스텀 구분자 추출
    커스텀 구분자를 문자열 상단의 `//`와 `\n` 사이에서 추출할 수 있도록 함수를 정의함.
    정규식 내에서 개행 문자(`\n`)가 실제 줄바꿈으로 인식되지 않도록 백슬래시(`\`)를 추가함.
    
    콜론(:) 오타를 수정함.

--- src/main/java/calculator/InputManager.java
+++ src/main/java/calculator/InputManager.java
@@ -1,12 +1,15 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InputManager {
 
     public enum DefaultSeparator {
         COMMA(","),
-        COLONE(":");
+        COLON(":");
         final private String value;
         DefaultSeparator(String value) {
             this.value = value;
@@ -18,7 +21,7 @@ public class InputManager {
 
     public enum CustomSeparatorWrapper {
         DOUBLE_SLASH("//"),
-        NEW_LINE("\n");
+        NEW_LINE("\\n");
         final private String value;
         CustomSeparatorWrapper(String value) {
             this.value = value;
@@ -26,5 +29,27 @@ public class InputManager {
         public String getValue() {
             return value;
         }
+    }
+    
+    private String buildCustomSeparatorRegex() {
+        StringBuilder regexBuilder = new StringBuilder();
+        regexBuilder.append("^");
+        regexBuilder.append(CustomSeparatorWrapper.DOUBLE_SLASH.getValue());
+        regexBuilder.append("(.)\\");
+        regexBuilder.append(CustomSeparatorWrapper.NEW_LINE.getValue());
+
+        return regexBuilder.toString();
+    }
+    
+    private String extractCustomSeparator(String validatedStr) {
+        // 커스텀 구분자 추출용 정규식 생성 (문자열 앞 //와 \n 사이 문자)
+        String regexForCustomSeparator = buildCustomSeparatorRegex();
+        // 정규식에 해당하는 커스텀 구분자 추출
+        Pattern pattern = Pattern.compile(regexForCustomSeparator);
+        Matcher matcher = pattern.matcher(validatedStr);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return "";
     }
 }
