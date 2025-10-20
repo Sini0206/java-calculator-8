b113d86a1fa874f966f7a88221ec89577df7200b
Sini0206 <a01092439672@gmail.com>
2025-10-20 21:04:51 +0900

    test: 예외 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -17,10 +17,66 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 예외_테스트_음수_입력_불가() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_구분자_제외한_문자_입력_불가() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(":1a,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_0_입력_불가() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_문자열_맨_앞_커스텀_구분자_패턴_부재() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("4//!\n2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_문자열_부정확한_커스텀_구분자_패턴_위치() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//!\n4//*\n2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_기본_구분자_커스텀_구분자_혼용() {
+        assertSimpleTest(() -> {
+            run("//;\\n:1,4:;10");
+            assertThat(output()).contains("결과 : 15");
+        });
+    }
+
+    @Test
+    void 예외_테스트_구분자_부재() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1/4/10"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트__커스텀_구분자_패턴_사이_복수_문자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;;\\n:1,4:;10"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
